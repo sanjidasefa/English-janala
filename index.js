e3cf91f033d198b0422fb705a36957165afdcc74
sanjidasefa <sanjidasefa9@gmail.com>
2025-10-04 15:35:16 +0600

    update js functionality

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+//load lesson
 const lesson = ()=>{
   fetch('https://openapi.programming-hero.com/api/levels/all')
   .then(res=>res.json())
   .then(data=> lessonName(data.data))
+ 
 }
 lesson()
 
@@ -10,7 +12,7 @@ const loadWord = (id)=>{
   fetch(url).then(res=>res.json()).then(data=>wordDetils(data.data))
 }
 
-
+//display lessons
 const lessonName = (ele)=>{
   const id = document.getElementById('lesson')
   id.innerHTML="";
@@ -21,7 +23,6 @@ const lessonName = (ele)=>{
     </div>
     `
   });
-  
 }
 
 const lessonData = (id) =>{
@@ -65,7 +66,7 @@ const word = (word)=>{
       <h2>${meaning ? meaning: 'meaning not found'} / ${pronunciation}</h2>
       <div class="flex justify-between mx-5 pb-5">
         <h1 onclick="loadWord(${element.id})"><i class="fa-solid fa-circle-exclamation"></i></h1>
-        <h1><i class="fa-solid fa-xmark"></i></h1>
+        <h1 onclick="pronounceWord('${pronunciation}')"><i class="fa-solid fa-volume-high"></i></h1>
       </div>
       </div>
     </div>
@@ -75,8 +76,54 @@ const word = (word)=>{
 
 const wordDetils = (word)=>{
   const details = document.getElementById('modal')
+  details.innerHTML=""
   details.innerHTML+=`
-  <h1>hello</h1>
+ <div>
+ <h1 class="text-3xl font-semibold">${word.word} (${word.pronunciation}) </h1>
+ <h3 class="text-2xl font-semibold my-3">meaning</h3>
+ <h2 class="text-2xl ">${word.meaning ? word.meaning : 'not found'}</h2>
+ <h3 class="text-2xl my-3 font-semibold">Example</h3>
+ <p>${word.sentence}</p>
+ <h1 class="text-2xl my-3">সমার্থক শব্দ গুলো</h1>
+ <div> ${showSynonyms(word.synonyms)}</div>
+ </div>
   `
- document.getElementById('modal_1)').showModal()
+ document.getElementById('modal_1').showModal();
 }
+
+const showSynonyms = (name)=>{
+  const element = name.map(ele=> `<span class="bg-slate-300 p-3">${ele}</span>`)
+  return element.join(" ")
+}
+
+//volume functionality
+ function pronounceWord(word) {
+      const utterance = new SpeechSynthesisUtterance(word);
+      utterance.lang = 'en-GB'; // English
+      window.speechSynthesis.speak(utterance);
+      console.log(window.speechSynthesis.getVoices());
+    }
+
+const spinner = (spin) =>{
+  if(spin == true){
+    document.getElementById('spinner').classList.remove('hidden')
+    document.getElementById('words').classList.add('hidden')
+  }
+  else{
+    document.getElementById('spinner').classList.add('hidden')
+    document.getElementById('words').classList.remove('hidden')
+  }
+}
+
+//search functionality
+document.getElementById('search').addEventListener("click", ()=>{
+  const input = document.getElementById('type');
+  const getInputValue = input.value.toLowerCase();
+  fetch('https://openapi.programming-hero.com/api/words/all')
+  .then(res=>res.json())
+  .then(data=>{
+    const datas = data.data;
+    const words = datas.filter(word => word.word.toLowerCase().includes(getInputValue))
+    word(words)
+  })
+})
